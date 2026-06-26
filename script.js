7ea40b2f8631b31124ce8e0ce751672731945e45
Abraham <158268924+abrahamcodes-dev@users.noreply.github.com>
2026-06-26 15:21:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-function showMessage() {
-    alert("Hello! JavaScript is working successfully.");
-}
+const video = document.getElementById("wildlifeVideo");
+const button = document.getElementById("videoButton");
+
+button.addEventListener("click", function () {
+    if (video.classList.contains("hidden")) {
+        video.classList.remove("hidden");
+        video.play();
+    } else if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+        video.classList.add("hidden");
+    }
+});
